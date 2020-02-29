e46493fdb2c8407631192c6f65977933a9f573a0
Lesta <a3885646@gmail.com>
2020-02-29 21:31:12 +0800

    test

---
+++ MySkills/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySkills.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+
+        public decimal BoxOffice { get; set; }
+
+        public String Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
