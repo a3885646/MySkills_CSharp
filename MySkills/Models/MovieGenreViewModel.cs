42db619f552ef3b9d3c4c7133cb5a552f997fb5a
Lesta <a3885646@gmail.com>
2020-03-01 17:01:34 +0800

    MovieGenreVM建立, CRUD基本功能建立

---
+++ MySkills/Models/MovieGenreViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MySkills.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public String MovieGenre { get; set; }
+        public String SearchString { get; set; }
+    }
+}
